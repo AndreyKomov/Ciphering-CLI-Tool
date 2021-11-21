56936a66422686d69a57bb0ffa3517f9282e298d
Andrei Komau <andrei_komau@epam.com>
2021-11-21 21:55:13 +0300

    feat: add test for streams files

--- tests/streams.test.js
+++ tests/streams.test.js
@@ -1,6 +1,8 @@
 const { expect } = require("@jest/globals");
 const { stderr } = require("process");
+const { Transform } = require("stream");
 const streams = require("../streams");
+const cesar = require('../cesar');
 
 test ('should return streams array', () => {
     process.argv = ['-c', 'C1-C0-R0-A'];
@@ -16,4 +18,15 @@ test ('should return cesar stream', () => {
     process.argv = ['-c', 'B'];
     expect(streams.cipherStreamsChain.cipherStreamsChain()).toEqual([]);
     expect(console.log).toHaveBeenCalled;
+})
+
+test ('should return cesar stream', () => {
+    process.argv = ['-c', 'C1'];
+    // const mockTransform = jest.spyOn(Transform._transform, 'method').mockImplementation(() => {});
+
+    const mockTransform = new Transform();
+    const result = mockTransform.push(cesar.cesar('This is secret. Message about "_" symbol!', 1));
+    // console.log(streams.cipherStreamsChain.cipherStreamsChain());
+    streams.cipherStreamsChain.cipherStreamsChain();
+    expect(result).toBe(true);
 })
